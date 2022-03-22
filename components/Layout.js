e762820bdb2eed587211e6943b4cc276080bb333
Louis Jeon <louisjeon@admins-MacBook-Pro.local>
2022-03-22 13:03:56 +0900

    #2

---
+++ components/Layout.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import NavBar from "./NavBar";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <NavBar />
+      <div>{children}</div>
+    </>
+  );
+}
---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import SEO from "../components/SEO";
+
+export default function Potato() {
+  return (
+    <div>
+      <SEO title="about" />
+      <h1>About</h1>
+    </div>
+  );
+}
